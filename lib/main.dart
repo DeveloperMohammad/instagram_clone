8140332e6547e6852e5b1078e3d8fb0efe8bb777
DeveloperMohammad <linuxfan576@gmail.com>
2022-06-06 06:22:18 +0430

    setup firebase for mobile and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:instagram/responsive/mobile_screen_layout.dart';
+import 'package:instagram/responsive/responsive_layout_screen.dart';
+import 'package:instagram/responsive/web_screen_layout.dart';
 import 'package:instagram/utils/colors.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: mobileBackgroundColor,
+void main() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyChdKq9u8hQ9nva0EdxdFx4xfndc16EY4o',
+        appId: '1:1057962218091:web:ba5ddbec7fc3ff5d55d72e',
+        messagingSenderId: '1057962218091',
+        projectId: 'instagram-6c09a',
+        storageBucket: 'instagram-6c09a.appspot.com',
       ),
-      home: const MyApp(),
-    ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(
+    const MyApp(),
   );
 }
 
@@ -19,11 +30,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Wow! Hot reload works now just fine and fast'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: mobileBackgroundColor,
+      ),
+      home: const ResponsiveLayout(
+        mobileScreenLayout: MobileScreenLayout(),
+        webScreenLayout: WebScreenLayout(),
       ),
     );
   }
 }
-
