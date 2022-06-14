2cf02580d27bad737195386b8244128f4e97447a
DeveloperMohammad <linuxfan576@gmail.com>
2022-06-14 20:51:53 +0430

    added bottom navigation bar of type cupertino Items to easily navigate between screens

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,13 +1,15 @@
 // Dart Imports
 
 //? Third party packages
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 //! User classes
-import 'package:instagram_clone/providers/user_provider.dart';
+// import 'package:instagram_clone/providers/user_provider.dart';
 import 'package:instagram_clone/resources/auth_methods.dart';
-import 'package:instagram_clone/models/user.dart' as model;
+import 'package:instagram_clone/utils/colors.dart';
+// import 'package:instagram_clone/models/user.dart' as model;
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
@@ -17,13 +19,36 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  int page = 0;
+  late PageController pageController;
+
+  void navTapped(int pageNumber) {
+    pageController.jumpToPage(pageNumber);
+  }
+
+  void onPageChanged(int pageNumber) {
+    setState(() => page = pageNumber);
+  }
+
+  @override
+  void initState() {
+    pageController = PageController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-  model.User user = Provider.of<UserProvider>(context).user;
+    // model.User user = Provider.of<UserProvider>(context).user;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(user.username),
+        title: const Text('user.username'),
         actions: [
           IconButton(
             onPressed: () async {
@@ -33,8 +58,51 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('This is mobile'),
+      body: PageView(
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+        children: const [
+          Center(child: Text('Home')),
+          Center(child: Text('Search')),
+          Center(child: Text('Post')),
+          Center(child: Text('Favorite')),
+          Center(child: Text('User')),
+        ],
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        activeColor: primaryColor,
+        inactiveColor: secondaryColor,
+        currentIndex: page,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_circle),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: '',
+            backgroundColor: primaryColor,
+          ),
+        ],
+        onTap: navTapped,
       ),
     );
   }
